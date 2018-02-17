fd26e29222b8573c4fe843defebd64afc334cc12
hasanunl <hu1997tr@gmail.com>
2018-02-17 20:41:48 +0300

    calculatorv2 olusturuldu.
    calculatorv2 olusturuldu ve calculations Class'ı eklendi.

---
+++ calculatorv2/calculatorv2/calculations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatorv2
+{
+    class calculations
+    {
+        public string add(double s1,double s2)
+        {
+            return (s1 + s2).ToString();
+        }
+
+        public string sub(double s1, double s2)
+        {
+            return (s1 - s2).ToString();
+        }
+
+        public string mul(double s1, double s2)
+        {
+            return (s1 * s2).ToString();
+        }
+
+        public string div(double s1, double s2)
+        {
+            return (s1 / s2).ToString();
+        }
+    }
+}
